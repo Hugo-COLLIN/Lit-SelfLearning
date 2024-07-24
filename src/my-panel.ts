3ac68dca03f8e5b4efba7cb22a8ccadcfdd1da1b
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-07-24 08:01:32 +0200

    03 00:44 - styling components

--- src/my-panel.ts
+++ src/my-panel.ts
@@ -1,11 +1,28 @@
-import {html, LitElement} from "lit";
+import {css, html, LitElement} from "lit";
 import {customElement} from "lit/decorators.js";
 
 @customElement("my-panel")
 export class MyPanel extends LitElement {
+  static styles = css`
+  .title {
+    background: aquamarine;
+    font-size: 20px;
+    font-weight: bold;
+      border-top-left-radius: 10px;
+  }
+      
+  .body {
+    padding: 1rem;
+    border: 2px solid darkorange;
+  }
+  `
+
   render() {
     return html`
-      <div>Hi there</div>
+      <div>
+        <div class="title"> TITLE</div>
+        <div class="body">CONTENT</div>
+      </div>
     `;
   }
 }
