9fe2d5ab87a64f788149edaf4d07589d3de0b4f9
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-07-24 08:19:34 +0200

    06 00:28 - boolean attributes

--- src/my-panel.ts
+++ src/my-panel.ts
@@ -3,6 +3,12 @@ import {customElement, property} from "lit/decorators.js";
 
 @customElement("my-panel")
 export class MyPanel extends LitElement {
+  @property({type: String})
+  title = "Default Title";
+
+  @property({type: Boolean})
+  opened = false;
+
   static styles = css`
   .title {
     background: aquamarine;
@@ -18,13 +24,12 @@ export class MyPanel extends LitElement {
   }
   `
 
-  @property({type: String}) title = "Default Title";
 
   render() {
     return html`
       <div>
         <div class="title">
-          ${this.title}
+          ${this.title} - ${this.opened ? "Opened" : "Closed"}
         </div>
         <div class="body">
           <slot>Default Content</slot>
